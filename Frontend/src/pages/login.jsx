d8fa617a9e32b8c2182f7762b5e3bcace4024078
Maisha50 <153121800+Maisha50@users.noreply.github.com>
2025-02-02 03:28:18 +0600

    changes

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -1,8 +1,25 @@
 import react from 'react';
+import './Login.css';
+
+
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='wrapper'>
+            <form action="" >
+                <h1>Login</h1>
+                <div className="input_box">
+                    <input type="text" placeholder='Username'required/>
+                </div>
+                <div className="input_box">
+                    <input type="password" placeholder='Password'required/>
+                </div>
+           <button type="submit" >Login</button>
+           <div className="register-link">
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>    
+            </form>
+        </div>
     );
-}
+};
 
 export default Login;
