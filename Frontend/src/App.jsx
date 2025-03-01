abc88a9a6ad15332019c94955764f3bb2adeaddd
Lamisa-51 <122662374+Lamisa-51@users.noreply.github.com>
2025-02-28 22:02:41 -0800

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Register from "./pages/register";
 import Login from "./pages/login";
-import ProfilePage from "./pages/ProfilePage"; 
+import ProfilePage from "./pages/ProfilePage";
+import TuitionList from "./pages/TuitionList";
+import TuitionDetails from "./pages/TuitionDetails";
 import TutorRequest from "./pages/tutorReq"; 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/tutor-request" element={<TutorRequest />} />
+        <Route path="/find-tutions" element={<TuitionList />} />
+        <Route path="/tuition/:id" element={<TuitionDetails />} />
         <Route path="/profile" element={<ProfilePage />} /> 
   
 
